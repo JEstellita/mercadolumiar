a94ce6bd02e557fa7f90fa71a8aa193c2dc8466f
joaoestellita <contato.jrod@gmail.com>
2025-09-09 21:32:30 -0300

    100

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
   site: process.env.PUBLIC_SITE_URL || 'https://mercadobiorregional.vercel.app',
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     sitemap()
   ]
 });
